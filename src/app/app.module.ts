31f501765ba234a59eb86864f5ab09078d226bce
JunyaMasuto <hukurou82@gmail.com>
2021-06-04 19:35:24 +0900

    コミット

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductComponent } from './product/product.component';
+// import { ProductListComponent } from './product/product-list/product-list.component';
+// import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+// import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
